c91bb4f476397c139eed482639ecaf6580a7627d
yuridevweb <yuridevweb@gmail.com>
2022-09-10 19:37:05 +0100

    copy reg and auth api from another repo

---
+++ django-rest-authentication/base/api/views.py
@@ -0,0 +1,71 @@
+from django.http import JsonResponse
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from .serializers import NoteSerializer
+from base.models import Note
+
+from rest_framework import generics
+from .serializers import RegistrationSerializer
+from rest_framework import serializers
+from rest_framework import status
+import uuid
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        # ...
+
+        return token
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        '/api/token',
+        '/api/token/refresh',
+    ]
+
+    return Response(routes)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getNotes(request):
+    user = request.user
+    notes = user.note_set.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
+
+
+class RegistrationAPIView(generics.GenericAPIView):
+
+    serializer_class = RegistrationSerializer
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        if (serializer.is_valid()):
+            serializer.save()
+            return Response({
+                "RequestId": str(uuid.uuid4()),
+                "Message": "User created successfully",
+
+                "User": serializer.data}, status=status.HTTP_201_CREATED
+            )
+
+        # return Response({"Errors": serializers.errors}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"Errors": 'Username is minimum 6 characters long \n Email must be unique'}, status=status.HTTP_400_BAD_REQUEST)

011300c780df73ba15efc503ce23255ac54e018f
yuridevweb <yuridevweb@gmail.com>
2022-09-18 21:14:57 +0100

    Change production DB settings

--- maximal-murdle-api/backend/settings.py
+++ maximal-murdle-api/backend/settings.py
@@ -19,7 +19,7 @@ import dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-#.env variables 
+# .env variables
 dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
     dotenv.load_dotenv(dotenv_file)
@@ -131,12 +131,27 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    """ Development DB settings """
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    """ Production DB settings """
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'murdle_api',
+            'USER': 'murdle_admin',
+            'PASSWORD': 'murdle',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+
+    }
 
 
 # Password validation
